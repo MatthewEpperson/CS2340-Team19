aaa5885f03d426d0043c2c08b392e22b91b86ab8
Matthew Epperson <MatthewREpperson@gmail.com>
2022-06-07 16:27:55 -0400

    Added ability to cycle games on game menu

--- GamesSuite/Assets/Scripts/StartMenu.cs
+++ GamesSuite/Assets/Scripts/StartMenu.cs
@@ -1,24 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class StartMenu : MonoBehaviour
 {
-    public void QuitGame() {
+    private TMP_Text currGame;
+    private string[] gamesList = {"Uno", "Wordle", "Go"};
+    private int gamesListIndex = 0;
+    void Start() {
+        currGame = GameObject.Find("ChangingPanel/ChooseGame/SelectedGame").GetComponent<TMP_Text>();
+    }
+
+    public void quitGame() {
         Debug.Log("QUIT!");
         Application.Quit();
     }
 
-    public void Credits() {
+    public void credits() {
         Debug.Log("credits");
     }
 
-    public void Settings() {
+    public void settings() {
         Debug.Log("settings");
     }
 
-    public void Games() {
+    public void games() {
         Debug.Log("games");
+    }
+
+    public void nextGame() {
+        if (gamesListIndex + 1 >= gamesList.Length) {
+            gamesListIndex = 0;
+        } else {
+            gamesListIndex++;
+        }
+        currGame.text = gamesList[gamesListIndex];
+    }
+
+    public void prevGame() {
+        if (gamesListIndex - 1 < 0) {
+            gamesListIndex = gamesList.Length - 1;
+        } else {
+            gamesListIndex--;
+        }
+        currGame.text = gamesList[gamesListIndex];
     }
 }
