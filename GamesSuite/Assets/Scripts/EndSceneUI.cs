ce214e0437c7d3cd8359ad843f6e8779cf59c133
wgreene22 <wgreene32@gatech.edu>
2022-06-29 21:23:24 -0400

    Wordle end screen makeover

--- GamesSuite/Assets/Scripts/EndSceneUI.cs
+++ GamesSuite/Assets/Scripts/EndSceneUI.cs
@@ -7,6 +7,22 @@ using UnityEngine.SceneManagement;
 public class EndSceneUI : MonoBehaviour
 {
     private TMP_Text winLoseText;
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    private TMP_Text winLoseText2;
+    void Start() {
+        winLoseText = GameObject.Find("Win/Lose Text").GetComponent<TMP_Text>();
+        winLoseText2 = GameObject.Find("Win/Lose Text 2").GetComponent<TMP_Text>();
+        if (WordlePlayer.playerWin) {
+            winLoseText.text = "Congratulations!";
+            winLoseText2.text = "You Guessed The Word";
+                                
+        } else {
+            winLoseText.text = "Better Luck Next Time";
+            winLoseText2.text = "The correct word was: " + WordlePlayer.correctWord.ToUpper();
+=======
+>>>>>>> Stashed changes
     void Start() {
         winLoseText = GameObject.Find("Win/Lose Text").GetComponent<TMP_Text>();
         if (WordlePlayer.playerWin) {
@@ -14,6 +30,10 @@ public class EndSceneUI : MonoBehaviour
         } else {
             winLoseText.text = "You LOST. \n" +
                                 "The correct word was: " + WordlePlayer.correctWord.ToUpper();
+<<<<<<< Updated upstream
+=======
+>>>>>>> be7bb3546e76cdbd8d7c8b3b0e00e42ab5a95fea
+>>>>>>> Stashed changes
         }
     }
     
