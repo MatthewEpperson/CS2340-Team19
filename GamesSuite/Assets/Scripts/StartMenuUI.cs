5f8fc65279203fdcf9099a6cf875f39b0390cda6
Matthew Epperson <MatthewREpperson@gmail.com>
2022-07-01 02:42:12 -0400

    Added animations to buttons

---
+++ GamesSuite/Assets/Scripts/StartMenuUI.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuUI : MonoBehaviour
+{
+
+    Transform leftPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftPanel = GameObject.Find("LeftPanel").GetComponent<Transform>();
+        
+        foreach (Transform child in leftPanel) {
+            StartCoroutine(floatGameObjects(child));
+        }
+    }
+
+
+
+    IEnumerator floatGameObjects(Transform obj) {
+
+        float speed = 10f;
+        while (true) {
+            Vector3 oldPos = obj.transform.position;
+
+            float obj_x = obj.transform.position.x;
+            float obj_y = obj.transform.position.y;
+            float obj_z = obj.transform.position.z;
+            
+            while (obj_y > (oldPos.y - 5f)) {
+                obj_y -= speed * Time.deltaTime;
+                obj.transform.position = new Vector3(obj_x, obj_y, obj_z);
+                yield return null;
+            }
+
+            while (obj_y < (oldPos.y)) {
+                obj_y += speed * Time.deltaTime;
+                obj.transform.position = new Vector3(obj_x, obj_y, obj_z);
+                yield return null;
+            }
+
+            obj.transform.position = oldPos;
+        }
+    }
+}
